e805baf0037d136d805d0169c855182169d222a7
ismail <coderismail22@gmail.com>
2024-09-22 22:13:09 +0600

    update server and config index

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 const main = async () => {
   try {
