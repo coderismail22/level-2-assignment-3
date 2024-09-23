dcfb2a418d8d8e6feb06b0cf0765376bc5582624
ismail <coderismail22@gmail.com>
2024-09-23 06:49:36 +0600

    make routes scalable

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import express, { Application, Request, Response, application } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express, { Application, Request, Response, application } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import router from './routes';
 
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5000/api/v1"] }));
+app.use(cors({ origin: ['http://localhost:5000/api/v1'] }));
 
+app.use('/api/v1',router);
 // use
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
