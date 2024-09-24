1d9447bf7a002f25a3145fcc3d01d9c3ec9b10ce
ismail <coderismail22@gmail.com>
2024-09-24 22:13:36 +0600

    add validation for sign in and sign up

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,19 @@
-import express from 'express';
-import { AuthControllers } from './auth.controller';
+import express from "express";
+import { AuthControllers } from "./auth.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { AuthValidations } from "./auth.validation";
 
 const router = express.Router();
 
-router.post('/signup', AuthControllers.signUp);
-router.post('/signin', AuthControllers.signIn);
+router.post(
+  "/signup",
+  validateRequest(AuthValidations.userSignUpValidationSchema),
+  AuthControllers.signUp,
+);
+router.post(
+  "/signin",
+  validateRequest(AuthValidations.userLoginValidationSchema),
+  AuthControllers.signIn,
+);
 
 export const Auth = router;
